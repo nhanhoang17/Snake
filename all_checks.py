2abde4e4d43a6150face742885feae111368c808
Nhan hoang <me@example.com>
2025-10-07 00:16:34 +0700

    update all_checks.py with new check

--- all_checks.py
+++ all_checks.py
@@ -1,13 +1,15 @@
-
 #!/usr/bin/env python3
 
 import os
+import sys
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
-    return os.path.exists("/run/reboot-required")	
+    return os.path.exists("/run/reboot-required")
 
 def main():
-    pass
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
 
 main()
